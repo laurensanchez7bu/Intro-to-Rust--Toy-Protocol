5bfdf3cab02d1fa545fe306c94d8a8d296027eeb
laurensanchez7bu <Lauren_Sanchez4@baylor.edu>
2026-01-30 03:14:04 +0000

    Finished serialize/deserialize functions and added additional error tests!

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,9 @@ pub enum UserError {
     #[error("The packet field is too long to be serialized.")]
     FieldTooLarge,
 
+    #[error("The packet field is too small to be serialized.")]
+    FieldTooSmall,
+
     #[error("The body of the packet has invalid data.")]
     MalformedBody,
 
@@ -41,19 +44,80 @@ pub enum UserError {
 impl User {
     // Serialize the User struct to a Vec<u8> in big endian format
     fn serialize(&self) -> Result<Vec<u8>, UserError> {
-        /*
-        TODO:
-        Your code here
-         */
+        if self.access_level > 31 {
+            return Err(UserError::FieldTooLarge);
+        }
+
+        let namebytes = self.name.as_bytes();
+        let emailbytes = self.email.as_bytes();
+
+        if namebytes.is_empty() || emailbytes.is_empty() {
+            return Err(UserError::FieldTooSmall);
+        }
+
+        if namebytes.len() > u8::MAX as usize || emailbytes.len() > u8::MAX as usize {
+            return Err(UserError::FieldTooLarge);
+        }
+
+        let mut output = Vec::<u8>::new();
+        let activebit: u8 = if self.active { 1 } else { 0 };
+        let headerbyte: u8 = (self.access_level << 3) | (activebit << 2);
+
+        output.write_u16::<BigEndian>(self.id)?;
+        output.write_u8(headerbyte)?;
+
+        output.write_u8(namebytes.len() as u8)?;
+        output.extend_from_slice(namebytes);
+        output.write_u8(emailbytes.len() as u8)?;
+        output.extend_from_slice(emailbytes);
+
+        Ok(output)
     }
 
     /// Deserialize a Vec<u8> (in big endian format) back to a User struct. Returns an error
     /// if the packet is invalid.
     fn deserialize(buf: &[u8]) -> Result<User, UserError> {
-        /*
-        TODO:
-        Your code here
-         */
+        if buf.len() < 3 {
+            return Err(UserError::MalformedBody);
+        }
+
+        let mut cursor = Cursor::new(buf);
+
+        let map_eof = |e: io::Error| {
+            if e.kind() == io::ErrorKind::UnexpectedEof {
+                UserError::MalformedBody
+            } else {
+                UserError::IoError(e)
+            }
+        };
+
+        let id = cursor.read_u16::<BigEndian>().map_err(map_eof)?;
+
+        let b = cursor.read_u8().map_err(map_eof)?;
+        let access_level = b >> 3;
+        let active = ((b >> 2) & 0b1) == 1;
+
+        let namelength = cursor.read_u8().map_err(map_eof)? as usize;
+        if namelength == 0 {
+            return Err(UserError::MalformedBody);
+        }
+        let mut namebuff = vec![0u8; namelength];
+        cursor.read_exact(&mut namebuff).map_err(map_eof)?;
+        let name = String::from_utf8(namebuff).map_err(|_| UserError::MalformedBody)?;
+
+        let emaillength = cursor.read_u8().map_err(map_eof)? as usize;
+        if emaillength == 0 {
+            return Err(UserError::MalformedBody);
+        }
+        let mut emailbuff = vec![0u8; emaillength];
+        cursor.read_exact(&mut emailbuff).map_err(map_eof)?;
+        let email = String::from_utf8(emailbuff).map_err(|_| UserError::MalformedBody)?;
+
+        if (cursor.position() as usize) != buf.len() {
+            return Err(UserError::MalformedBody);
+        }
+
+        Ok(User { id, access_level, active, name, email })
     }
 }
 
--- src/test.rs
+++ src/test.rs
@@ -6,6 +6,7 @@ use super::*;
 #[cfg(test)]
 mod test {
     use crate::User;
+    use crate::UserError;
 
     fn create_valid_user() -> User {
         let user = User {
@@ -35,9 +36,91 @@ mod test {
         assert_eq!(user, deserialized_user);
     }
 
-    /*
-    TODO:
-    Add more tests here, and document what each one does. You should cover all cases where you are
-    trying to serialize or deserialize invalid data.
-     */
+    /// Serialize a user with an invalid access level
+    #[test]
+    fn test_serialize_user_invalid_access_level_too_large() {
+        let mut user = create_valid_user();
+        user.access_level = 40;
+        let result = user.serialize();
+        assert!(matches!(result, Err(UserError::FieldTooLarge)));
+    }
+
+    /// Serialize a user with an invalid name length
+    #[test]
+    fn test_serialize_user_invalid_name_too_large() {
+        let mut user = create_valid_user();
+        user.name = "A".repeat(300);
+        let result = user.serialize();
+        assert!(matches!(result, Err(UserError::FieldTooLarge)));
+    }
+
+    /// Serialize a user with an invalid email length
+    #[test]
+    fn test_serialize_user_invalid_email_too_large() {
+        let mut user = create_valid_user();
+        user.email = "A".repeat(300);
+        let result = user.serialize();
+        assert!(matches!(result, Err(UserError::FieldTooLarge)));
+    }
+
+    /// Serialize a user with an invalid name (empty)
+    #[test]
+    fn test_serialize_user_invalid_name_field_too_small() {
+        let mut user = create_valid_user();
+        user.name = "".to_string();
+        let result = user.serialize();
+        assert!(matches!(result, Err(UserError::FieldTooSmall)));
+    }
+
+    /// Serialize a user with an invalid email (empty)
+    #[test]
+    fn test_serialize_user_invalid_email__field_too_small() {
+        let mut user = create_valid_user();
+        user.email = "".to_string();
+        let result = user.serialize();
+        assert!(matches!(result, Err(UserError::FieldTooSmall)));
+    }
+
+    /// Deserialize a user with an invalid header (too short)
+    #[test]
+    fn test_deserialize_user_invalid_header_malformed_body() {
+        let buf: Vec<u8> = vec![0x00, 0x01];
+        let result = super::User::deserialize(&buf);
+        assert!(matches!(result, Err(UserError::MalformedBody)));
+    }
+
+    /// Deserialize a user with invalid name (empty)
+    #[test]
+    fn test_deserialize_user_invalid_name_malformed_body() {
+        let user = create_valid_user();
+        let mut buf = user.serialize().unwrap();
+
+        buf[3] = 0;
+        let result = super::User::deserialize(&buf);
+        assert!(matches!(result, Err(UserError::MalformedBody)));
+    }
+
+    /// Deserialize a user with invalid email (empty)
+    #[test]
+    fn test_deserialize_user_invalid_email_malformed_body() {
+        let user = create_valid_user();
+        let mut buf = user.serialize().unwrap();
+
+        let namelength = buf[3] as usize;
+        let emaillengthindex = 4 + namelength;
+
+        buf[emaillengthindex] = 0;
+        let result = super::User::deserialize(&buf);
+        assert!(matches!(result, Err(UserError::MalformedBody)));
+    }
+
+    /// Deserialize a user with invalid length of body (too long)
+    #[test]
+    fn test_deserialize_user_invalid_body_malformed_body() {
+        let user = create_valid_user();
+        let mut buf = user.serialize().unwrap();
+        buf.push(0xAA);
+        let result = super::User::deserialize(&buf);
+        assert!(matches!(result, Err(UserError::MalformedBody)));
+    }
 }
